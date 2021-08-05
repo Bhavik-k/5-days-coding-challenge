f444d1e368063895945a3f5c25bb600151e016e2
Bhavik Khawale <62886971+Bhavik-k@users.noreply.github.com>
2021-08-05 09:27:58 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDvDGmGP1CGYoxqeWubt26hnUkWejtQfC0",
+    authDomain: "mama-zon-b60c3.firebaseapp.com",
+    projectId: "mama-zon-b60c3",
+    storageBucket: "mama-zon-b60c3.appspot.com",
+    messagingSenderId: "1019631040421",
+    appId: "1:1019631040421:web:f4b74c2fa0d16584201a4c",
+    measurementId: "G-TQC1C6VKHQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const scrollrightss = document.getElementsByClassName("rightScroll")
 
 for (let i = 0; i < scrollLeftss.length; i++) {
     const scrollLeft = scrollLeftss[i];
+    
+    setInterval(function () {
+        ChangeDisplay(scrollLeft);
+    },500);
+    console.log('working');
     scrollLeft.addEventListener("click", function (e) {
         const DivToScrollLeft = e.target.parentElement;
         DivToScrollLeft.scrollLeft = DivToScrollLeft.scrollLeft-DivToScrollLeft.offsetWidth
@@ -12,8 +17,35 @@ for (let i = 0; i < scrollLeftss.length; i++) {
 }
 for (let i = 0; i < scrollrightss.length; i++) {
     const scrollLeft = scrollrightss[i];
+    
+    setInterval(function () {
+        ChangeDisplayY(scrollLeft);
+    },500);
+
     scrollLeft.addEventListener("click", function (e) {
         const DivToScrollLeft = e.target.parentElement;
         DivToScrollLeft.scrollLeft = DivToScrollLeft.scrollLeft+DivToScrollLeft.offsetWidth
     })
+}
+
+
+function ChangeDisplay(scrollLeft) {
+    if (scrollLeft.parentElement.scrollLeft == 0) {
+        // console.log(scrollLeft.parentElement.scrollLeft);
+        scrollLeft.style.display = 'none';
+    }else{
+        scrollLeft.style.display = 'flex';
+
+    }
+}
+function ChangeDisplayY(scrollLeft) {
+    console.log(scrollLeft.parentElement.scrollLeft);
+    console.log(scrollLeft.scrollWidth-scrollLeft.offsetWidth);
+    if (scrollLeft.parentElement.scrollLeft == scrollLeft.scrollWidth-scrollLeft.offsetWidth){
+        // console.log(scrollLeft.parentElement.scrollLeft);
+        scrollLeft.style.display = 'none';
+    }else{
+        scrollLeft.style.display = 'flex';
+
+    }
 }
